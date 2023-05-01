4418fc7510a0b5ea9703f7374a346c10274c9102
Minul Sandith <106053448+MinulSandith@users.noreply.github.com>
2023-05-01 17:32:51 +0530

    Add files via upload

---
+++ pages/1_Chat_with_Minul.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header(":sunglasses: Chat with Minul")
+st.subheader(
+    "This is yet under development.It will be available soon!"
+)
+st.sidebar.header(":sunglasses: Chat with Minul")
